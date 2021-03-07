9168f52b7213c8de238938312f80fd3e30c5a2f1
Hansel Wei <hello@hanselwei.dev>
2021-03-06 23:41:53 -0500

    Code cleanup

--- docker-dependencies/install-data-science-tools.sh
+++
@@ -1,6 +0,0 @@
-apt-get update -y && apt-get -y upgrade &&\ 
-	wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh &&\ 
-	sha256sum Anaconda3-2019.03-Linux-x86_64.sh | echo &&\
-	sudo chmod +x Anaconda3-2019.03-Linux-x86_64.sh &&\
-	./Anaconda3-2019.03-Linux-x86_64.sh &&\
-	source ~/.bashrc
